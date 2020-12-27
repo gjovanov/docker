d17e2e1cb036c11a078e799f25669ad489049613
gjovanov <goran.jovanov@gmail.com>
2020-12-27 20:27:46 +0100

    coturn 0.2.17

--- coturn/scripts/docker_entrypoint.sh
+++ coturn/scripts/docker_entrypoint.sh
@@ -2,7 +2,7 @@
 
 # If command starts with an option, prepend with turnserver binary.
 if [ "${1:0:1}" == '-' ]; then
-  set -- turnserver "$@"
+  set -- /opt/coturn/turnserver "$@"
 fi
 
 exec $(eval "echo $@")
